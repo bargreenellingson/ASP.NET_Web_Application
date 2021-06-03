5cb62ff4bde5d5d61076f7ea484bf5fd3f4fc66c
Jonathan Velazquez <jvelaz07@uw.edu>
2021-06-03 12:07:49 -0700

    first commit

---
+++ WebApplication/Controllers/CustomerController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("Customer")]
+    public class CustomerController : ControllerBase
+    {
+    }
+}
